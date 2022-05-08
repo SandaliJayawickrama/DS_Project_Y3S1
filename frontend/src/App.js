db9fdab16063265322ed423b0a3a64e4d123986e
Chethana Prasadi <it20065294@my.sliit.lk>
2022-05-08 23:46:27 +0530

    create cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,12 @@
 import './styles/Navbar.css'
+import './styles/buyer.css'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
 import Navbar from "./components/Navbar";
-import { Home } from './components/Home';
+import Home from './components/Home';
+import { ItemDisplay } from './components/ItemDisplay';
+import Delivery from './components/Delivery';
 
 
 function App() {
@@ -10,6 +15,8 @@ function App() {
       <Navbar/>
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/cart" element={<ItemDisplay />} />
+          <Route exact path="/delivery" element={<Delivery />} />
         </Routes>
     </>
   );
