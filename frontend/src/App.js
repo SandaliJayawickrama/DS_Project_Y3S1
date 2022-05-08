3a32e0e5ae963065a7c08a8a9be641776d4a6b25
Sandali Jayawickrama <it20095062@my.sliit.lk>
2022-05-09 03:22:06 +0530

    pull from main

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,30 +4,18 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
 import Navbar from "./components/Navbar";
-
-import { Home } from './components/Home';
-import AddItems_Farmers from './components/AddItems_Farmers';
-import HomeFarmer from './components/HomeFarmer';
-
 import Home from './components/Home';
 import { ItemDisplay } from './components/ItemDisplay';
 import Delivery from './components/Delivery';
 import MobilePayMain from './components/MobilePay/MobilePayMain';
-
+import AddItems_Farmers from './components/AddItems_Farmers';
+import HomeFarmer from './components/HomeFarmer';
 
 
 function App() {
   return (
     <>
       <Navbar/>
-      
-      <Routes>
-          <Route exact path="/about" element={<Home/>} />
-          <Route exact path="/FarmerHome" element={<HomeFarmer/>} />
-          <Route exact path="/addItems" element={<AddItems_Farmers/>} />
-      </Routes>
-      
-      
         <Routes>
           <Route exact path="/" element={<Home />} />
 
@@ -35,6 +23,8 @@ function App() {
           <Route exact path="/delivery" element={<Delivery />} />
           <Route path="/bill" element={<MobilePayMain />} />
 
+          <Route exact path="/FarmerHome" element={<HomeFarmer/>} />
+          <Route exact path="/addItems" element={<AddItems_Farmers/>} />
         </Routes>
     </>
   );
